9f85bd86c201954b839dd69926f3119c74c941da
sunsunarto <sunaryo24259@outlook.com>
2025-05-22 13:00:36 +0700

    Second  commit

--- app/Components/AboutHome.jsx
+++ app/Components/AboutHome.jsx
@@ -4,8 +4,13 @@ import "../ComponentStyle/AboutHome.css";
 function AboutHome() {
     return (
         <div className="about">
-            <div>hello</div>
-            <img src="/projects.png" alt="Projects" />
+            <div className="container">
+                <div className="text">
+                    <h2>your Art Space </h2>
+                    <p>Your space is where colors, typography, and layout blend to create stunning visuals.<br/>we offer a range of services to help you bring your ideas to life, including design, development, and deployment.</p>
+                </div>
+                 <img src="/projects.png" alt="Projects" />
+            </div>
         </div>
     );
 }
---
+++ app/Components/Project.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function Project() {
+    return(
+        <div className="projectCon">
+            <div className="continer">
+                <div className="projectAbout index1">
+                    <h1>Socio</h1>
+                    <div className="githubAndLink">
+                        <div className="github">
+                            <a href="https://github.com/sunsunarto/Quiz_16_Sunaryo/">Github Link</a>
+                        </div>
+                        <div className="link">
+                            <a href="https://quiz-16-sunaryo.vercel.app/"> Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectAbout index2">
+                    <h1>Cerita Anekdot</h1>
+                    <div className="githubAndLink">
+                        <div className="github">
+                            <a href="https://github.com/sunsunarto/Projekbi">Github Link</a>
+                        </div>
+                        <div className="link">
+                            <a href="https://projekbi-3i6x.vercel.app"> Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectAbout index3">
+                    <h1>Dessert shop</h1>
+                    <div className="githubAndLink">
+                        <div className="github">
+                            <a href="https://github.com/sunsunarto/Dessert-Shop-Part-2">Github Link</a>
+                        </div>
+                        <div className="link">
+                            <a href="https://dessert-shop-part-2.vercel.app/"> Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="projectAbout index4">
+                    <h1>Post Management App</h1>
+                    <div className="githubAndLink">
+                        <div className="github">
+                            <a href="https://github.com/sunsunarto/Post-Management-App">Github Link</a>
+                        </div>
+                        <div className="link">
+                            <a href="https://post-management-app-939m.vercel.app/"> Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Project;
