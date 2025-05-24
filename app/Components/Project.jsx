7c9388680520854f80736041ca9d7042eeab67c7
sunsunarto <sunaryo24259@outlook.com>
2025-05-24 18:38:23 +0700

    fix bug in project commit

--- app/Components/Project.jsx
+++ app/Components/Project.jsx
@@ -19,7 +19,7 @@ function Project() {
                         trigger: el,
                         start: "top center",
                         toggleActions: "play none none reverse",
-                        onLeaveBack: () => gsap.to(el, { opacity: 0, x: -100, duration: 1 })
+                        onLeaveBack: () => gsap.to(el, { opacity: 0, y: -100, duration: 1 })
                     },
                     delay: index * 0.3,
                 }
